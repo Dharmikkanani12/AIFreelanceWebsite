9af6a7b390ef3205ca0b22e3e2330b4789a54890
Dharmik Kanani <152086922+Dharmikkanani12@users.noreply.github.com>
2025-05-23 16:36:38 +0000

    changed

--- aiconnect_site/aiconnect/views.py
+++ aiconnect_site/aiconnect/views.py
@@ -1,10 +1,3 @@
-"""
-from django.shortcuts import render
-
-def home(request):
-    return render(request, "home.html")
-"""
-
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
